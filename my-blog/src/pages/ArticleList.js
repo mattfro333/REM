d050e0984a972c14b5d05e1a34babb6e46d38121
mattfro333 <mattfro333@gmail.com>
2020-02-19 15:15:51 -0700

    added link function with a map to articles list

--- my-blog/src/pages/ArticleList.js
+++ my-blog/src/pages/ArticleList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import articleContent from './article-content';
 
 const ArticlesList = () => (
   <>
-
     <h1>Articles</h1>
+    {articleContent.map((article, key) => (
+       <Link className="list-item" key={key} to={`/article/${article.name}`}>
+       <h3>{article.title}</h3>
+       </Link>
+    ))}
   </>
 );
 
