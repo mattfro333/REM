bd21812dc9f8398a3bfe94c262d825d1f772de81
mattfro333 <mattfro333@gmail.com>
2020-02-19 14:34:09 -0700

    map over article content

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import articleContent from './article-content';
+
 const ArticlePage = ({match}) => {
-const name = match.params.name;
-// const article = articleContent.find(article => article.name === name);
-//
-//   if(!article) return <h1>Article does not exist!</h1>
+  const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
+
+      if(!article) return <h1>Article does not exist!</h1>
 
 return(
 
   <>
-    <h1>{name}</h1>
-
+    <h1>{article.title}</h1>
+    {article.content.map((paragraph, key) => (
+      <p key={key}>{paragraph}</p>
+    ))}
   </>
 );
 }
