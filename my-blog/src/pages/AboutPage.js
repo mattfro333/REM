6df3116d24ce18d21146abef0dd399a7dd74a708
mattfro333 <mattfro333@gmail.com>
2020-02-22 15:33:21 -0700

    added content to about page

--- my-blog/src/pages/AboutPage.js
+++ my-blog/src/pages/AboutPage.js
@@ -3,7 +3,10 @@ import React from 'react';
 const AboutPage = () => (
   <>
 
-    <h1>Hello, Welcog</h1>
+    <h1>About Us</h1>
+    <h4>"Us ......... and them ....... when we're all just ordinary men..... In ....... Out..... what's the fuss is all about"</h4>
+    <h5>-Pink Floyd</h5>
+    <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p>
   </>
 );
 
