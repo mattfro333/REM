2f6bdd5b445c2de82632d83006fd86c91da812f7
mattfro333 <mattfro333@gmail.com>
2020-02-29 01:51:51 -0700

    added post for upvotes checked end point

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -10,7 +10,7 @@ app.get('/api/articles/:name', async (req, res) => {
    try {
        const articleName = req.params.name;
 
-       const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true})
+       const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true});
        const db = client.db('my-blog');
 
        const articleInfo = await db.collection('articles').findOne({ name: articleName});
@@ -20,22 +20,32 @@ app.get('/api/articles/:name', async (req, res) => {
    } catch (error) {
         res.status(500).json({ message: 'Error connecting to db', error });
    }
-   const articleName = req.params.name;
+});
 
-   const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true})
-   const db = client.db('my-blog');
-
-   const articleInfo = await db.collection('articles').findOne({ name: articleName});
-   res.status(200).json(articleInfo);
-
-   client.close();
-})
-
-app.post('/api/articles/:name/upvote', (req, res) => {
+app.post('/api/articles/:name/upvote', async (req, res) => {
+try {
   const articleName = req.params.name;
 
-  articlesInfo[articleName].upvotes += 1;
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+  const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true});
+  const db = client.db('my-blog');
+
+  const articleInfo = await db.collection('articles').findOne({ name: articleName });
+  await db.collection('articles').updateOne(
+    { name: articleName },
+      {
+        $set: {
+        upvotes: articleInfo.upvotes + 1,
+      },
+    },
+  );
+  const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+  res.status(200).json(updatedArticleInfo);
+
+  client.close();
+} catch (error) {
+     res.status(500).json({ message: 'Error connecting to db', error });
+}
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
