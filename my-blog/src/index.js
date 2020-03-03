05bd1523f87a4cb0e208c44a5e279c77cd584f33
mattfro333 <mattfro333@gmail.com>
2020-03-03 15:26:17 -0700

    the fetch with useEffect is flags errors and coming back null

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,7 +1,9 @@
+import 'whatwg-fetch'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import './index.css';
+
 
 ReactDOM.render(
   <App />,
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList'
 import NotFoundPage from './NotFoundPage'
 const ArticlesListPage = ({match}) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
+
+useEffect(async() => {
+  const fetchData = async () => {
+    const result = await fetch(`/api/articles/${name}`)
+    const body = await result.json();
+    console.log(body)
+    setArticleInfo(body);
+  }
+  fetchData();
+}, [name]);
 
       if(!article) return <NotFoundPage/>
 
@@ -14,6 +26,7 @@ return(
 
   <>
     <h1>{article.title}</h1>
+    <p>This post has been upvoted {articleInfo.upvotes} times </p>
     {article.content.map((paragraph, key) => (
       <p key={key}>{paragraph}</p>
     ))}
